25f6b7821ce949621ecb73777563f4db9a7ef51b
3zzazakl <vanzekovichgr@gmail.com>
2023-10-31 19:28:30 +0200

    combine

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,7 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:02d}{:02d}, ".format(i, j), end='')
-print()
+        if (i == 8) and (j == 9):
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}, ".format(i, j), end='')
