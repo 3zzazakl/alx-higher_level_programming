43c307eb8e0f1b4fe83d7ac76d723246133ef0b0
3zzazakl <vanzekovichgr@gmail.com>
2023-11-05 18:08:52 +0200

    print matrox

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        if len(row) > 0:
-            print(*row, sep=" ")
-        else:
-            print("")
+    if matrix is not None:
+        for row in matrix:
+            if len(row) > 0:
+                for col in row:
+                    print("{:d}".format(col), end=" ")
+                print("")
+            else:
+                continue
