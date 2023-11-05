9ea5c91bf79dbf23e49b413c9c8c4aae58aac950
3zzazakl <vanzekovichgr@gmail.com>
2023-11-05 18:32:30 +0200

    fix spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is not None:
         for row in matrix:
-            if len(row) > 0:
-                for i, col in enumerate(row):
-                    end = " " if i != len(row) - 1 else ""
-                    print("{:d}".format(col), end=end)
-                print("")
-        print()
+            for i, col in enumerate(row):
+                end = " " if i != len(row) - 1 else ""
+                print("{:d}".format(col), end=end)
+            print("")
