c2375d2f140c32d6992e07c9c62ad625ff719d76
3zzazakl <vanzekovichgr@gmail.com>
2023-11-05 19:06:29 +0200

    adding a new test for none

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence is None:
-        sentence[0] = ""
+    if len(sentence) == 0:
+        return (0, None)
     else:
         return (len(sentence), sentence[0])
