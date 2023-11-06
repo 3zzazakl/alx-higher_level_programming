624d7cfe17ec8b7e6c1ca4867619e14710d0b386
3zzazakl <vanzekovichgr@gmail.com>
2023-11-07 00:23:26 +0200

    check sandbox

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include "Python.h"
+
+/**
+ * print_python_list_info - print info about lists.
+ * @p: Pointer to python object.
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+	PyListObject *ll;
+	Py_ssize_t list_size, i;
+
+	ll = (PyListObject *) p;
+	list_size = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %ld\n", list_size);
+
+	printf("[*] Allocated = %ld\n", ll->allocated);
+
+	for (i = 0; i < list_size; i++)
+	{
+		printf("Element %ld\n", i);
+		printf("%s\n", ll->ob_item[i]->ob_type->tp_name);
+	}
+}
