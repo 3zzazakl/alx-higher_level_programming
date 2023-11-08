fa2a6a8c73ba901784ddaf42cba6cc7e6a48102c
3zzazakl <vanzekovichgr@gmail.com>
2023-11-09 01:32:24 +0200

    delete key

--- 0x04-python-more_data_structures/8-main.py
+++ 0x04-python-more_data_structures/8-main.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from re import A
-
-
 simple_delete = __import__('8-simple_delete').simple_delete
 print_sorted_dictionary = \
 __import__('6-print_sorted_dictionary').print_sorted_dictionary
