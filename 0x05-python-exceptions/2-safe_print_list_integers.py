99f4d1cccd77938741d3c42753731c0dd1450d7b
3zzazakl <vanzekovichgr@gmail.com>
2023-11-21 00:30:23 +0200

    print list of integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,8 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
             except IndexError:
                 pass
     except IndexError:
-        print()
-        return (count)
+        pass
     finally:
         print()
         return (count)
