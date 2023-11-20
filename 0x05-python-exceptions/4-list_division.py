a94493309662709dbcc428b529432ede920fa16c
3zzazakl <vanzekovichgr@gmail.com>
2023-11-21 00:00:33 +0200

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,8 +2,8 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     nwlist = []
-    result = 0
     for i in range(list_length):
+        result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
