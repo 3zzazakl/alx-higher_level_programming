d633b504a303c53900e9dcd2de0c0c3921bea93d
3zzazakl <vanzekovichgr@gmail.com>
2023-11-21 23:23:08 +0200

    adding private variable

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Write a class Square that defines a square
+
+    Args:
+        size: an empty variable
+    """
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size
