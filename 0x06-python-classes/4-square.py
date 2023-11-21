ab6e4b810730d6bebcc80f67b12e1034f8281df0
3zzazakl <vanzekovichgr@gmail.com>
2023-11-22 00:16:20 +0200

    check value

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Area of a square
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
+
+
+class Square:
+    """Area of a square
+
+    Attributes:
+        __size (int): the size of square
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    def area(self):
+        return self.__size ** 2
