e3050cec2d417582223a2bc49943d4538b073b66
3zzazakl <vanzekovichgr@gmail.com>
2023-11-21 23:46:53 +0200

    returen square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Area of a square
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
+
+
+class Square:
+    """Area of a square
+    
+    Attributes:
+        __size (int): the size of square
+
+    Raises:
+        TypeError: _description_
+        ValueError: _description_
+    """
+
+    __size = None
+    area = None
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return self.__size ** 2
