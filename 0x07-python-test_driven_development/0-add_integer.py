17e88f59dbdd8da4d9f72f0693193ecb8f621e96
3zzazakl <vanzekovichgr@gmail.com>
2023-11-26 21:41:33 +0200

    setting env for first task

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    add_integer_to_: this is function to add integer
+"""
+
+
+def add_integer(a, b=98):
+    """
+        adding two numbers
+    :param a: first number
+    :param b: second number
+    :return: summation of the two numbers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer or float")
+    if not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or float")
+    a = int(a)
+    b = int(b)
+    return a + b
