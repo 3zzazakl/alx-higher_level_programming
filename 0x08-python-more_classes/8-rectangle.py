088b2026091ab81902f13566dca92f3982e56622
3zzazakl <vanzekovichgr@gmail.com>
2023-11-29 18:13:11 +0200

    bigger or not

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -78,7 +78,8 @@ class Rectangle:
         "delete message"
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
-
+        
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
 
         if not isinstance(rect_1, Rectangle):
