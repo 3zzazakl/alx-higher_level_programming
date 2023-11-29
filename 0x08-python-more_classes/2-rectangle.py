6b550dae47ff8571d4ee2e08309d0f49727cba53
3zzazakl <vanzekovichgr@gmail.com>
2023-11-29 15:07:51 +0200

    check rect

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+setting a rectangle class for task.
+"""
+
+
+class Rectangle:
+    """
+    rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        """Initializes rectangle instance with width and height."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieve width of rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width of rectangle instance"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__witdth = value
+
+    @property
+    def height(self):
+        """retrieve height of rectangle instance"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height of rectangle instance"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """return the area of rectangle"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """return the perimeter of rectangle."""
+        return 2 * (self.width + self.height) \
+            if self.width != 0 and self.heigh != 0 else 0
