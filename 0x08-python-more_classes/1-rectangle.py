eda5b963324730a4f541bd3ef669384673e89cb0
3zzazakl <vanzekovichgr@gmail.com>
2023-11-29 15:20:43 +0200

    fix variable name

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
         elif value < 0:
             raise TypeError("width must be >= 0")
         else:
-            self.__witdth = value
+            self.__width = value
 
     @property
     def height(self):
