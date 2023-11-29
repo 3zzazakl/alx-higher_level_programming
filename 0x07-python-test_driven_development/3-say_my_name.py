b1d0b43c29899ce9201a546fa185c6d04af763bf
3zzazakl <vanzekovichgr@gmail.com>
2023-11-29 02:26:16 +0200

    passing failure

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""
-"""
+"""Defines a name function."""
 
 
 def say_my_name(first_name, last_name=""):
+    """"Define a name function
+
+    Keyword arguments:
+    first_name -- first argument
+    second -- second argument
+    Return: return_description
+    """
     if not isinstance(first_name, str) or not isinstance(last_name, str):
         raise TypeError("first_name must be a string")
     if last_name:
--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-"""
-"""
+"""define a square function."""
 
 
 def print_square(size):
+    """printing a square with # character.
+
+    Args:
+        size (int): the height/width of the square
+
+    Raises:
+        TypeError: if size isn't integer.
+        ValueError: if size is < 0
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
     for i in range(size):
-        print("#" * size)
+        [print("#", end="") for j in range(size)]
+        print("")
