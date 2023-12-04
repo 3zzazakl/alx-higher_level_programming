a0f7ba068b929191d487589da9006e0a7f0450e2
3zzazakl <vanzekovichgr@gmail.com>
2023-12-05 00:47:06 +0200

    check task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""write a function to print an exception message.
+
+    Raises:
+        Exception: _description_
+    """
+
+
+class BaseGeometry:
+    """class
+    """
+    def area(self):
+        """function
+
+        Raises:
+            Exception: _description_
+        """
+        raise Exception("area() is not implemented")
