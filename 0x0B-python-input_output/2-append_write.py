ee9fd9961cd724d1c6690db69deb7c6e618e10de
3zzazakl <vanzekovichgr@gmail.com>
2023-12-05 22:08:56 +0200

    append text

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+def append_write(filename="", text=""):
+    """_summary_
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        count = f.write(text)
+    return count
