0202fc2938ab8f10a8b89f1c3188b611b52e5ccd
3zzazakl <vanzekovichgr@gmail.com>
2023-12-05 23:12:20 +0200

    fix errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""_summary_
-"""
+"""_summary_."""
 import sys
-import os
+
 save_to_json = __import__("5-save_to_json_file").save_to_json_file
 load_from_json = __import__("6-load_from_json_file").load_from_json_file
 
 
-def add_item(args, filename):
-    """_summary_
-
-    Args:
-        args (_type_): _description_
-        filename (_type_): _description_
-    """
-
-    if (os.path.exists(filename)):
-        content = load_from_json(filename)
-    else:
-        content = []
-    content.extend(args)
-    save_to_json(content, filename)
+if __name__ == "__main__":
+    try:
+        my_list = load_from_json("add_item.json")
+    except FileNotFoundError:
+        my_list = []
+    my_list += sys.argv[1:]
+    save_to_json(my_list, "add_item.json")
