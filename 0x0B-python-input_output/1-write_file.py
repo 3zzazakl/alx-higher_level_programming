fbd1168dfbb3b03561fbd92e5791cb9345a74988
3zzazakl <vanzekovichgr@gmail.com>
2023-12-05 22:01:55 +0200

    write a text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+"""_summary_
+"""
 
+
+def write_file(filename="", text=""):
+    """write a string to text
+
+    Args:
+        filename (str, optional): _description_. Defaults to "".
+        text (str, optional): _description_. Defaults to "".
+
+    Returns:
+        _type_: _description_
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        count = f.write(text)
+
+    return count
