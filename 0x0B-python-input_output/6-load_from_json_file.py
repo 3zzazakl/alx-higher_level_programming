96df3aad0bb2311c3e01853760e439c148fd4dd1
3zzazakl <vanzekovichgr@gmail.com>
2023-12-05 22:22:55 +0200

    load from josn

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """_summary_
+
+    Args:
+        filename (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    with open(filename, encoding="utf-8") as f:
+        my_obj = json.loads(f.read())
+    return my_obj
