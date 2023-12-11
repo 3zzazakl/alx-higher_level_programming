e46fec2fbbff3f07d1eaf1eeee1e83b57e55220c
3zzazakl <vanzekovichgr@gmail.com>
2023-12-12 00:17:06 +0200

    check task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -139,10 +139,13 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.x, self.y, self.width, self.height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """_summary_
         """
         attrs = ['id', 'width', 'height', 'x', 'y']
         if args:
             for i, arg in enumerate(args):
                 setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
