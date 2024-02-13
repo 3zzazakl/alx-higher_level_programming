fb575b66fe7d20f5d3b0fa3251f3b3b51abec4e2
3zzazakl <vanzekovichgr@gmail.com>
2024-02-13 04:12:55 +0200

    checking task8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,18 +2,14 @@
 
 const size = parseInt(process.argv[2]);
 
-if (!isNaN(size)) {
-  if (size > 0) {
-    for (let i = 0; i < size; i++) {
-      let row = '';
-      for (let ii = 0; ii < size; ii++) {
-        row += 'x';
-      }
-      console.log(row);
-    }
-  } else {
-    console.log('Missing size');
-  }
-} else {
+if (isNaN(size) || size < 1) {
   console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    const row = '';
+    for (let ii = 0; ii < size; ii++) {
+      line += 'X';
+    }
+    console.log(row);
+  }
 }
