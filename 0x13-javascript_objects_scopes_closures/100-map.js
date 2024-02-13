06c1c6bc978b300694811021d339a74bc64339f4
3zzazakl <vanzekovichgr@gmail.com>
2024-02-13 12:57:28 +0200

    change reserved word

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 
 const { list } = require('./100-data');
 
-const new = list.map((value, index) => value * index);
+const newlst = list.map((value, index) => value * index);
 
 console.log(list);
-console.log(new);
+console.log(newlst);
