07b24ee4df452f313d390fefa435bf96105ead2d
3zzazakl <vanzekovichgr@gmail.com>
2024-02-13 02:58:46 +0200

    fixing task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2)
-{
-    console.log('No argument');
-}
-else if (process.argv.lenght === 3)
-{
-    console.log('Argument found');
-}
-else
-{
-    console.log('Argument found');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.lenght === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
