03132bf85329829acdae5654783ee7860d26667c
3zzazakl <vanzekovichgr@gmail.com>
2024-02-18 22:30:05 +0200

    checking task13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """_summary_
+    """
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    dbs = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(user, passwd, dbs),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    f_state = session.query(State).filter(State.name.like('%a%')).all()
+    for state in f_state:
+        session.delete(state)
+        session.commit()
+    session.close()
