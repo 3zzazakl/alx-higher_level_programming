ebd3f3c25b00635945a7649a73fafdb5e9a0f84e
3zzazakl <vanzekovichgr@gmail.com>
2024-02-18 04:47:10 +0200

    checking task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+"""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """_summary_
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'Ne%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
