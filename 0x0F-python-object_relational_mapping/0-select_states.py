cd6d1f135481ae59638bbba1c01a2108ad8eeea8
3zzazakl <vanzekovichgr@gmail.com>
2024-02-18 04:42:47 +0200

    checking task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Connecting to mysql database and listing all states
 """
-"""
+
 import MySQLdb
 import sys
 
@@ -8,7 +9,7 @@ import sys
 if __name__ == "__main__":
     """_summary_
     """
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
