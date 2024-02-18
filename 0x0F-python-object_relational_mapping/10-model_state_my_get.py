1a433c996c13a557248144550da2af010cfdce7f
3zzazakl <vanzekovichgr@gmail.com>
2024-02-18 07:19:08 +0200

    checking task10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@\
+        localhost:3306/{}'.format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]))
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    f_state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if f_state:
+        print(f"{f_state.id}")
+    else:
+        print("Not found")
+    session.close()
