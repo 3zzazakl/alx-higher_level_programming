52992ebe5737dc34f51c90e44f3567b680989812
3zzazakl <vanzekovichgr@gmail.com>
2024-02-18 06:35:11 +0200

    checking task6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = MetaData()
+
+
+class State(Base):
+    """_summary_
+
+    Args:
+        Base (_type_): _description_
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
