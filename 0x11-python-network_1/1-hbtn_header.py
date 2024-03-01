71f4c469a30e109205fc59ebb25fe4a0244e49dd
3zzazakl <vanzekovichgr@gmail.com>
2024-03-02 01:02:29 +0200

    checking task1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+import urllib.request as urq
+import sys
+
+
+url = sys.argv[1]
+
+req = urq.Request(url)
+with urq.urlopen(url) as response:
+    id = response.getheader('X-Request-Id')
+    print(id)
