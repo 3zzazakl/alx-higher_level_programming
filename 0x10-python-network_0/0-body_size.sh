4988bdb4f6a2deca043514c6a919e3c43bbcfd8d
3zzazakl <vanzekovichgr@gmail.com>
2024-03-01 03:07:23 +0200

    checking task0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a request to that URL
 
-curl -sI "$1" | grep -i Content-Length | cut -d' ' -f2
+curl -sI "$1" | grep -i Content-Length | sed 's/[^0-9]*//g'
