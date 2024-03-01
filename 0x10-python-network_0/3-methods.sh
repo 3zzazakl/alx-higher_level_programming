d16aedfb1055c3bb6dd0503c961a4260532787a4
3zzazakl <vanzekovichgr@gmail.com>
2024-03-01 02:43:19 +0200

    checing task

---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Write a Bash script that takes in a URL and displays all HTTP methods
+
+curl -sI "$1" | grep -i Allow | cut -d' ' -f2-
---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Write a Bash script that takes in a URL as an argument, sends a GET request
+
+curl -sH "X-School-User-Id: 98" "$1"
