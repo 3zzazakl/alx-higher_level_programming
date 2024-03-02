f36ea606ff51edc399977c1c26ab193f185e4d46
3zzazakl <vanzekovichgr@gmail.com>
2024-03-02 02:33:56 +0200

    checking task100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""_summary_
+"""
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    """_summary_
+    """
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            print(commit.get('sha'), commit.get('commit').
+                  get('author').get('name'))
+    else:
+        print("None")
