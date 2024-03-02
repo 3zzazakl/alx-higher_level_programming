7c6f837ca26b4f7d1c42e6ccce67bd40b7fe406f
3zzazakl <vanzekovichgr@gmail.com>
2024-03-02 02:21:23 +0200

    checking task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    load = {'q': q}
+    response = requests.post(url, load)
+
+    try:
+        json_res = response.json()
+        if json_res:
+            print(f"[{json_res['id']}] {json_res['name']}")
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
