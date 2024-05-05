709e19c984614282aa56a257941e8981106f5e2d
3zzazakl <vanzekovichgr@gmail.com>
2024-05-05 23:04:22 +0300

    checking task100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieID = process.argv[2];
+
+if (!movieID) {
+  console.error('Missing argument');
+  process.exit(1);
+}
+
+const apiURL = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+
+request(apiURL, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
