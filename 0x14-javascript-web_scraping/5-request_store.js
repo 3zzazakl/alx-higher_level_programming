f80ca910aca99de895383a2bacaba4d11c957499
3zzazakl <vanzekovichgr@gmail.com>
2024-05-05 22:54:35 +0300

    checking task5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+if (!url || !filePath) {
+  console.error('Missing arguments');
+  process.exit(1);
+}
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error('Invalid status code <' + response.statusCode + '>');
+  } else {
+    fs.writeFile(filePath, body, function (err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('The file was saved!');
+      }
+    });
+  }
+});
