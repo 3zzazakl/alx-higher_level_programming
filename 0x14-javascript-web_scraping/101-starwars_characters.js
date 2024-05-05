0a51d29d67a81d88be9d3649fbee37f178069a77
3zzazakl <vanzekovichgr@gmail.com>
2024-05-05 23:20:12 +0300

    fix request

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,21 +8,49 @@ if (!movieID) {
   process.exit(1);
 }
 
-const apiURL = 'https://swapi-api.hbtn.io/api/films/' + movieID;
+const apiURL = `https://swapi-api.hbtn.io/api/films/${movieID}/`;
+
+function getCharacter (url, callback) {
+  request(url, function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error('Invalid status code <' + response.statusCode + '>');
+      return;
+    }
+    callback(JSON.parse(body).name);
+  });
+}
 
 request(apiURL, function (error, response, body) {
   if (error) {
     console.error(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
+    process.exit(1);
+  }
+  if (response.statusCode !== 200) {
+    console.error('Invalid status code <' + response.statusCode + '>');
+    process.exit(1);
+  }
+
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+
+  function fetchCharacter (urls) {
+    let completedRequests = 0;
+    const characterNames = new Array(urls.length);
+
+    urls.forEach((url, index) => {
+      getCharacter(url, function (name) {
+        characterNames[index] = name;
+        completedRequests++;
+        if (completedRequests === urls.length) {
+          characterNames.forEach(name => console.log(name));
         }
       });
-    }
+    });
   }
+  fetchCharacter(characterUrls);
 });
