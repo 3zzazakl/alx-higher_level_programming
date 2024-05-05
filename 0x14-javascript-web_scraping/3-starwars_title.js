5efd8cab8361e5e97db1c7dfefe2d4d8c98f87af
3zzazakl <vanzekovichgr@gmail.com>
2024-05-05 05:16:27 +0300

    sem

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,11 +5,10 @@ const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${MovieId}`;
 
 request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    }
-    else {
-        const movie = JSON.parse(body);
-        console.log(movie.title);
-    }  
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
 });
